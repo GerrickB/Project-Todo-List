8f54456da9db2c325a96571c4941cb76eb6cb010
GerrickB <27402037+GerrickB@users.noreply.github.com>
2023-10-07 21:00:21 -0700

    Add load-form and todo-holder

---
+++ src/load-form.js
@@ -0,0 +1,11 @@
+import TodoItem from './todo-item.js';
+
+function loadForm() {
+  // cancel and submit form reloads add task button
+  const addTask = document.querySelector('.add-task');
+  addTask.remove();
+  
+  const form = document.createElement
+}
+
+export default loadForm;
---
+++ src/todo-holder.js
@@ -0,0 +1,17 @@
+
+
+// use this class to store
+class TodoHolder {
+  constructor(name) {
+    this.name = name;
+    this.todos = [];
+  }
+
+  addTodo(todoItems) {
+    if (todoItem instanceof TodoItem) {
+      this.todos.push(todoItem); 
+    } else {
+      throw new Error('Invalid todo item');
+    }
+  }
+}
