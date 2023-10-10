d2894682e558a94a559a4657d8d1b4dd28e94021
GerrickB <27402037+GerrickB@users.noreply.github.com>
2023-10-10 00:47:57 -0700

    Import todoHolder into index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
 import './styles.css';
 import loadHeader from './load-header.js';
 import loadDefault from './load-default.js';
+// maybe import todo-holder here
+import TodoHolder from './todo-holder.js';
 
 function startup() {
+  const todoHolder = new TodoHolder()
   loadHeader();
-  loadDefault();
+  loadDefault(todoHolder);
   //try adding tasks first without todo class object
 }
 
 startup();
-//import todo-holder here
